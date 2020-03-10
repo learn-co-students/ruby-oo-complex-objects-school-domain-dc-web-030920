f3b6726a842dbdcb5cbb662bdfb166fe2070041a
Dawit Gizaw <gizawdawit123.com>
2020-03-10 17:40:34 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,42 @@
 # code here!
+class School
+    
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    attr_accessor :roster
+
+    def roster
+        @roster
+    end
+
+    def add_student(student, grade)
+        if @roster[grade]
+            @roster[grade] << student
+        else
+            @roster[grade] = [student]
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        
+        @roster.each do|key, value| 
+                @roster[key] = value.sort
+        end
+
+
+        
+    end
+
+    
+
+    
+
+
+end
