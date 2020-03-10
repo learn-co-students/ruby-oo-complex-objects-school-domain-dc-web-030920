e28eafe1f6464486900497de583a5fd149d25ca3
bret-gibson <bret.gibson@gmail.com>
2020-03-10 17:36:09 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+class School
+
+    def initialize(roster)
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        if @roster.has_key?(grade)
+            @roster[grade] << name
+        else 
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+    end
+
+    def grade(grade_num)
+        if @roster.has_key?(grade_num)
+            grade_val = @roster[grade_num]
+        end
+    end
+
+    def sort
+        newHash = {}
+        @roster.sort.map do |grade, names|
+            newHash[grade] = names.sort
+        end
+        newHash
+    end
+end
